0bd0c35850c94730076f59bff78edd8ca8bd81f4
BURAK ATICI <aticibrk@gmail.com>
2023-08-27 18:57:22 +0300

    FlightSearchApp

---
+++ AmadeusFlightSearchApp/AmadeusFlightSearchApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AmadeusFlightSearchApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace AmadeusFlightSearchApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            
+            return View();
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+    }
+}
---
+++ AmadeusFlightSearchApp/AmadeusFlightSearchApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmadeusFlightSearchApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
